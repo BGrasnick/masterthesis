bf806e79916ce2966474ba9c05a8829440a8edc3
Bastien Grasnick <b.grasnick@gmail.com>
2017-09-25 17:12:05 +0200

    added comments on filtering data by disgenet genes

--- code/disgenet/filterDataByDisgenetGenes.py
+++ code/disgenet/filterDataByDisgenetGenes.py
@@ -6,16 +6,24 @@ def filterByDisgenetGenes(geneListLocation, dataLocation, outputLocation, geneNa
     df = pd.read_csv(dataLocation)
 
     allGenes = set()
+
+    # add column cancer_type
     allGenes.add(df.columns[0])
+
+    # this can be used for checking how many genes were found per disgenet name
     genesWithNumberOfFound = []
 
     for gene in geneList[0]:
-        matching = [s for s in df.columns if s.split(geneNameSeparator)[0] == gene]
+        matching = [geneName for geneName in df.columns if geneName.split(geneNameSeparator)[0] == gene]
+
+        # if we didn't find an exact match, look for subtypes
         if len(matching) == 0:
             matching = [s for s in df.columns if s.startswith(gene)]
+
         genesWithNumberOfFound.append((gene, len(matching)))
         allGenes.update(matching)
 
+    # filter the data so only columns with disgenet gene names remain
     filtered_df = df.filter(allGenes)
 
     filtered_df.to_csv(outputLocation, index=False)
