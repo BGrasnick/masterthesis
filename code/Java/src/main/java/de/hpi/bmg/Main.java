d1da9dde747dcd270daa292d082e3d6282123e89
Bastien Grasnick <b.grasnick@gmail.com>
2017-10-13 12:57:12 +0200

    added logging

--- code/Java/src/main/java/de/hpi/bmg/Main.java
+++ code/Java/src/main/java/de/hpi/bmg/Main.java
@@ -5,38 +5,53 @@ import weka.attributeSelection.AttributeSelection;
 import weka.core.Instances;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class Main {
+
+    private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
         Properties prop = loadProperties("config.properties");
 
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + "Starting loading data from " + prop.getProperty("inputFile"));
+
         DataLoader dl = new DataLoader(prop.getProperty("inputFile"));
 
         Instances data = dl.getData();
+
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + "Finished loading data from " + prop.getProperty("inputFile"));
 
         data.deleteAttributeAt(1);
 
         data.setClassIndex(0);
 
-        /*
-        String[] attributeSelectionMethods = {"SVM-RFE", "GainRatio", "InfoGain"};
+        String[] attributeSelectionMethods = {"ReliefF", "SVM-RFE", "GainRatio", "InfoGain"};
+
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Starting attribute selection with methods: " + String.join(",",attributeSelectionMethods));
 
         for (String asMethod : attributeSelectionMethods) {
             selectAttributes(data, asMethod, prop.getProperty("attributeRankingOutputFile"));
         }
-        */
 
-        String[] allMethods = {"SVM-RFE", "GainRatio", "InfoGain", "disgenetTop15", "disgenetTop25"};
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Finished attribute selection with methods: " + String.join(",",attributeSelectionMethods));
+
+        String[] allMethods = {"ReliefF", "SVM-RFE", "GainRatio", "InfoGain", "disgenetTop15", "disgenetTop25"};
 
         ClassificationEvaluator ce = new ClassificationEvaluator(data);
+
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Starting classification evaluation with models SMO, LR, NB, KNN3, KNN5 with k from " + prop.getProperty("topKmin") + " to " + prop.getProperty("topKmax"));
 
         for (String asMethod : allMethods) {
             classifyAndEvaluate(prop.getProperty("attributeRankingOutputFile") + asMethod + ".csv", ce,
                     Integer.parseInt(prop.getProperty("topKmin")), Integer.parseInt(prop.getProperty("topKmax")));
         }
+
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Finished classification evaluation with models SMO, LR, NB, KNN3, KNN5 with k from " + prop.getProperty("topKmin") + " to " + prop.getProperty("topKmax"));
 
         //selectAttributes(data, prop.getProperty("attributeSelection"), prop.getProperty("attributeRankingOutputFile"));
         //removeAttributes(data, prop.getProperty("attributeRankingOutputFile") + prop.getProperty("attributeSelection") + ".csv");
@@ -73,9 +88,14 @@ public class Main {
             writer.writeNext(header);
 
             for (int k = topKmin; k <= topKmax; k++) {
+
+                LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Starting classification evaluation with models SMO, LR, NB, KNN3, KNN5 with k of " + k);
+
                 String result = ce.trainAndEvaluateWithTopKAttributes(k, attributeRankingFileLocation);
                 writer.writeNext(result.split(","));
                 writer.flush();
+
+                LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Finished classification evaluation with models SMO, LR, NB, KNN3, KNN5 with k of " + k);
             }
 
             writer.close();
@@ -93,11 +113,15 @@ public class Main {
 
     private static void selectAttributes(Instances data, String attributeSelectionMethod, String attributeRankingOutputFile) {
 
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Starting attribute selection with method " + attributeSelectionMethod);
+
         AttributeSelector as = new AttributeSelector(data, attributeSelectionMethod);
 
         AttributeSelection attsel = as.selectAttributes();
 
         as.saveSelectedAttributes(attsel, attributeRankingOutputFile);
+
+        LOGGER.info(new SimpleDateFormat().format( new Date() ) + ": Finished attribute selection with method " + attributeSelectionMethod);
 
     }
 
