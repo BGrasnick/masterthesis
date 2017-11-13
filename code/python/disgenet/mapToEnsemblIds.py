97c482725e93c1f2008e482ffcccdee8fce75648
Bastien Grasnick <b.grasnick@gmail.com>
2017-11-13 15:48:34 +0100

    now works with offline data for mapping uniprot to ensembl

--- code/python/disgenet/mapToEnsemblIds.py
+++ code/python/disgenet/mapToEnsemblIds.py
@@ -1,32 +1,14 @@
-from urllib.parse import urlencode
-from urllib.request import urlopen, Request
 import glob, pdb
 import pandas as pd
 from collections import defaultdict
 
 
-def mapToEnsemblIds(geneDiseaseAssociationsLocation):
+def mapToEnsemblIds(geneDiseaseAssociationsLocation, uniProtToEnsemblMap):
 
     path = geneDiseaseAssociationsLocation + "*.tsv"
 
     for fname in glob.glob(path):
         df = pd.read_csv(fname, sep='\t')
-
-        geneUniprotIDList = df["c2.uniprotId"].tolist()
-
-        url = 'http://www.uniprot.org/uploadlists/'
-
-        params = {
-            'from': 'ACC',
-            'to': 'ENSEMBL_ID',
-            'format': 'tab',
-            'query': " ".join(geneUniprotIDList)}
-
-        data = urlencode(params).encode("utf-8")
-        request = Request(url, data)
-        request.add_header('User-Agent', 'Python')
-        response = urlopen(request)
-        page = response.read(200000)
 
         df_times = df["c2.uniprotId"].str.count(";") + 1
 
@@ -44,27 +26,19 @@ def mapToEnsemblIds(geneDiseaseAssociationsLocation):
                     lastName = elem["c2.symbol"]
                     lastIdx = 0
 
-                df.set_value(index, "c2.uniprotId", elem["c2.uniprotId"].split(";")[lastIdx].replace(".",""))
+                    df.at[index,"c2.symbol"] = elem["c2.uniprotId"].split(";")[lastIdx].replace(".","")
                 lastIdx += 1
-
-        uniProtEnsemblDict = defaultdict()
-
-        for entry in page.decode("utf-8").split("\n")[1:]:
-
-            if entry:
-                uniprot, ensembl = entry.split("\t")
-                uniProtEnsemblDict[uniprot] = ensembl
 
         indexes_to_drop = []
 
         for index, elem in df.iterrows():
-            if elem["c2.uniprotId"] in uniProtEnsemblDict.keys():
-                df.set_value(index, "c2.symbol", uniProtEnsemblDict[elem["c2.uniprotId"]])
+            if elem["c2.uniprotId"] in uniProtToEnsemblMap.keys():
+                df.at[index,"c2.symbol"] = uniProtToEnsemblMap[elem["c2.uniprotId"]][0]
             else:
-                df.set_value(index, "c2.symbol", "")
+                df.at[index,"c2.symbol"] = ""
                 indexes_to_drop.append(index)
 
         indexes_to_keep = set(range(df.shape[0])) - set(indexes_to_drop)
         df = df.take(list(indexes_to_keep))
 
-        df.to_csv(fname,  index=False, sep="\t")
+        df.to_csv(fname+"new",  index=False, sep="\t")
