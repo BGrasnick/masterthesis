e4567dfc0cba5467d782b618270c642392a57e80
Bastien Grasnick <b.grasnick@gmail.com>
2017-11-30 15:29:05 +0100

    updated rowVars for time measurements

--- code/R/calculateRowVarsRanking.R
+++ code/R/calculateRowVarsRanking.R
@@ -3,26 +3,24 @@ library(tictoc)
 
 rawData <- read.csv("../../data/GDC/TCGA-PRAD_TCGA-OV_TCGA-COAD_TCGA-LUSC_TCGA-BRCA_TCGA-PAAD_TCGA-STAD_TCGA-CESC__GeneExpressionQuantification_TP_HTSeq-Counts_WithDiseaseCodes.csv")
 
-tic("total")
+for (i in 1:10){
+  tic("total")
+  
+  geneExpressionMatrix <- rawData[-c(1,2)]
+  
+  rV <- rowVars(t(geneExpressionMatrix))
+  
+  ordered <- rV[order(-rV) , drop = FALSE]
+  
+  orderedNameList <- gsub("\\.", "|", names(ordered))
+  
+  orderedNameValueList <- c(orderedNameList,data.frame(ordered)[,1],order(rV, decreasing=TRUE))
+  
+  orderedNameValueMatrix <- matrix(orderedNameValueList,ncol=3)
+  
+  colnames(orderedNameValueMatrix) <- c("attributeName", "value", "index")
+  
+  toc()
+}
 
-tic("rowVars FS")
-
-geneExpressionMatrix <- rawData[-c(1,2)]
-
-rV <- rowVars(t(geneExpressionMatrix))
-
-toc()
-
-ordered <- rV[order(-rV) , drop = FALSE]
-
-orderedNameList <- gsub("\\.", "|", names(ordered))
-
-orderedNameValueList <- c(orderedNameList,data.frame(ordered)[,1],order(rV, decreasing=TRUE))
-
-orderedNameValueMatrix <- matrix(orderedNameValueList,ncol=3)
-
-colnames(orderedNameValueMatrix) <- c("attributeName", "value", "index")
-
-toc()
-
-write.csv(orderedNameValueMatrix, file = "../../data/rankedAttributes/GDCbad/rowVars.csv", row.names = FALSE)
+#write.csv(orderedNameValueMatrix, file = "../../data/rankedAttributes/GDCbad/rowVars.csv", row.names = FALSE)
